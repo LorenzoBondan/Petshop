8416b7f1072e10541237bd3dc6853a80b6860f08
LorenzoBondan <lbbondan@gmail.com>
2023-08-11 18:06:33 -0300

    ContactResourceTests

---
+++ backend/src/test/java/com/projects/petshop/resources/ContactResourceTests.java
@@ -0,0 +1,94 @@
+package com.projects.petshop.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projects.petshop.dto.ContactDTO;
+import com.projects.petshop.services.ContactService;
+import com.projects.petshop.services.exceptions.ResourceNotFoundException;
+import com.projects.petshop.tests.Factory;
+import com.projects.petshop.tests.TokenUtil;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ContactResourceTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ContactService service;
+	
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	private ContactDTO contactDTO;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	
+	private String username;
+	private String password;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		
+		existingId = 1L; 
+		nonExistingId = 2L;
+		
+		username = "000.123.456-78"; // Maria, admin
+		password = "123456";
+		
+		contactDTO = Factory.createContactDTO();
+		
+		Mockito.when(service.update(ArgumentMatchers.eq(existingId), ArgumentMatchers.any())).thenReturn(contactDTO);
+		Mockito.when(service.update(ArgumentMatchers.eq(nonExistingId), ArgumentMatchers.any())).thenThrow(ResourceNotFoundException.class);
+	}
+	
+	@Test
+	public void updateShouldReturnProductWhenIdExists() throws Exception {
+		
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, username, password);
+		
+		String jsonBody = objectMapper.writeValueAsString(contactDTO);
+		
+		mockMvc.perform(put("/contacts/{id}", existingId)
+				.header("Authorization", "Bearer " + accessToken)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").exists()); 
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+		
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, username, password);
+		
+		String jsonBody = objectMapper.writeValueAsString(contactDTO);
+		
+		mockMvc.perform(put("/contacts/{id}", nonExistingId)
+				.header("Authorization", "Bearer " + accessToken)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
+}
