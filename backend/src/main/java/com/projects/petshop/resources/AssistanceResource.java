ee75d8e631d4ce4298d5fc0d0e74203bf8e1adcc
LorenzoBondan <lbbondan@gmail.com>
2023-08-11 09:29:04 -0300

    AssistanceResource

---
+++ backend/src/main/java/com/projects/petshop/resources/AssistanceResource.java
@@ -0,0 +1,60 @@
+package com.projects.petshop.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projects.petshop.dto.AssistanceDTO;
+import com.projects.petshop.services.AssistanceService;
+
+@RestController
+@RequestMapping(value = "/assistances")
+public class AssistanceResource {
+	
+	@Autowired
+	private AssistanceService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<AssistanceDTO>> findAll(Pageable pageable){		
+		Page<AssistanceDTO> list = service.findAllPaged(pageable);	
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AssistanceDTO> findById(@PathVariable Long id) {
+		AssistanceDTO dto = service.findById(id);	
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<AssistanceDTO> insert (@RequestBody AssistanceDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);	
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<AssistanceDTO> update(@PathVariable Long id, @RequestBody AssistanceDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<AssistanceDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
