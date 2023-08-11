4b12387f48196af35aff812c44a8d20403dd3f6f
LorenzoBondan <lbbondan@gmail.com>
2023-08-11 10:20:15 -0300

    throwing an exception when client try to update an contact that is not from him

--- backend/src/main/java/com/projects/petshop/resources/ContactResource.java
+++ backend/src/main/java/com/projects/petshop/resources/ContactResource.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projects.petshop.dto.ContactDTO;
+import com.projects.petshop.entities.User;
+import com.projects.petshop.services.AuthService;
 import com.projects.petshop.services.ContactService;
+import com.projects.petshop.services.exceptions.UnauthorizedException;
 
 @RestController
 @RequestMapping(value = "/contacts")
@@ -18,8 +21,15 @@ public class ContactResource {
 	@Autowired
 	private ContactService service;
 	
+	@Autowired
+	private AuthService authService;
+	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ContactDTO> update(@PathVariable Long id, @RequestBody ContactDTO dto) {
+		User user = authService.authenticated();
+		if(!authService.isAdmin() && id != user.getClient().getContact().getId()) {
+			throw new UnauthorizedException("You can't update an contact that is not yours");
+		}
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
