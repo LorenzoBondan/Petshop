bfe9e39538789665afab7c072c2497a5b2a81e08
LorenzoBondan <lbbondan@gmail.com>
2023-08-12 11:44:25 -0300

    UserResource documentation

--- backend/src/main/java/com/projects/petshop/resources/UserResource.java
+++ backend/src/main/java/com/projects/petshop/resources/UserResource.java
@@ -28,6 +28,12 @@ import com.projects.petshop.services.AuthService;
 import com.projects.petshop.services.UserService;
 import com.projects.petshop.services.exceptions.ForbiddenException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "petshop-API")
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
@@ -38,6 +44,15 @@ public class UserResource {
 	@Autowired
 	private AuthService authService;
 	
+	@Operation(summary = "Get all the users", method = "GET")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successful search"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbbiden"),
+			@ApiResponse(responseCode = "422", description = "Invalid Data"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@GetMapping
 	public ResponseEntity<Page<UserDTO>> findAll(@RequestParam(value = "name", defaultValue = "") String name, Pageable pageable) {
@@ -45,6 +60,13 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@Operation(summary = "Get one user by cpf", method = "GET")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successful search"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@GetMapping(value = "/{cpf}") 
 	public ResponseEntity<UserDTO> findByCpf(@PathVariable String cpf) {
@@ -56,6 +78,15 @@ public class UserResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@Operation(summary = "Insert a new user", method = "POST")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successful insertion"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbbiden"),
+			@ApiResponse(responseCode = "422", description = "Invalid Data"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<UserDTO> insert (@Valid @RequestBody UserInsertDTO dto) {
@@ -65,6 +96,14 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(newDto);	
 	}
 	
+	@Operation(summary = "Update an user", method = "PUT")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Updated successfully"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "422", description = "Invalid Data"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@PutMapping(value = "/{cpf}")
 	public ResponseEntity<UserDTO> update(@PathVariable String cpf, @Valid @RequestBody UserUpdateDTO dto)	{
@@ -76,6 +115,15 @@ public class UserResource {
 		return ResponseEntity.ok().body(newDto);
 	}
 	
+	@Operation(summary = "Delete an user", method = "DELETE")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Deleted Successfully"),
+			@ApiResponse(responseCode = "400", description = "Integrity Violation"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbbiden"),
+			@ApiResponse(responseCode = "404", description = "Object not found"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@DeleteMapping(value = "/{cpf}")
 	public ResponseEntity<UserDTO> delete(@PathVariable String cpf){
